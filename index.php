62f5ccef32007f4888662b6aaffc9ad5ee649ce1
rbto <robertopalomino87@gmail.com>
2013-10-15 11:03:29 -0500

    Fixed: función duplicada en las funciones de blank theme

--- index.php
+++ index.php
@@ -125,29 +125,6 @@ if( !function_exists('cltvo_print_r') ){
 	}
 }
 
-/**
- * Remove standard image sizes so that these sizes are not
- * created during the Media Upload process
- *
- * Tested with WP 3.2.1
- *
- * Hooked to intermediate_image_sizes_advanced filter
- * See wp_generate_attachment_metadata( $attachment_id, $file ) in wp-admin/includes/image.php
- *
- * @param $sizes, array of default and added image sizes
- * @return $sizes, modified array of image sizes
- * @author Ade Walker http://www.studiograsshopper.ch
- */
-function sgr_filter_image_sizes( $sizes) {
-		
-	unset( $sizes['thumbnail']);
-	unset( $sizes['medium']);
-	unset( $sizes['large']);
-	
-	return $sizes;
-}
-//add_filter('intermediate_image_sizes_advanced', 'sgr_filter_image_sizes');
-
 //convierte int a string con signo de pesos, con punto decimal y dos ceros al final
 if( !function_exists('cltvo_dinero') ){
 	function cltvo_dinero( $numero ){
